764dfa5fd64f6d9d43105a373df0a08f5c070760
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-06-12 10:17:05 -0400

    Rename file to BeansApplication.java

--- src/test/java/com/oiab/beans/BeansApplicationTests.java
+++ src/test/java/com/oiab/beans/BeansApplicationTests.java
@@ -1,10 +1,10 @@
-package com.oiab.quickstart;
+package com.oiab.beans;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QuickstartApplicationTests {
+class BeansApplicationTests {
 
 	@Test
 	void contextLoads() {
