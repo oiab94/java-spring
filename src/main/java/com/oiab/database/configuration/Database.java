9570df8541740a7ab6b598b56ff1c0f5e316a0e9
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-06-18 11:32:07 -0400

    Creacion de JDBCTemplate para ejecutar queries

---
+++ src/main/java/com/oiab/database/configuration/Database.java
@@ -0,0 +1,21 @@
+package com.oiab.database.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Database {
+
+	/**
+	 * Permite ejecutar consultas SQL
+	 * @param dataSource
+	 * @return
+	 */
+	@Bean
+	public JdbcTemplate jdbcTemplate(final DataSource dataSource) {
+		return new JdbcTemplate();
+	}
+}
