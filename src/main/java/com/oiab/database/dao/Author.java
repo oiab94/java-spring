7cbe332b6c810148332889f8e3fc866c14be0502
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-06-18 11:53:48 -0400

    Esqueleto DAO de lo que será nuestra aplicación

---
+++ src/main/java/com/oiab/database/dao/Author.java
@@ -0,0 +1,4 @@
+package com.oiab.database.dao;
+
+public interface Author {
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors (
+    id      SERIAL PRIMARY KEY,
+    name    TEXT
+    age     INTEGER
+);
+
+CREATE TABLE books (
+    id          SERIAL PRIMARY KEY,
+    isbn        TEXT,
+    title       TEXT,
+    author_id   BIGINT CONSTRAINT fk_books_authros_id REFERENCES authors(id) ON DELETE CASCADE
+);
