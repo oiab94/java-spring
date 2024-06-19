9c8ae9f5d2abb1975c588f6a601e50ddb9d62b01
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-06-19 16:13:57 -0400

    IntegrationTest implementacion de integracion de test para Book

--- src/test/java/com/oiab/database/TestDataUtil.java
+++ src/test/java/com/oiab/database/TestDataUtil.java
@@ -1,6 +1,7 @@
 package com.oiab.database;
 
 import com.oiab.database.domain.Author;
+import com.oiab.database.domain.Book;
 
 public final class TestDataUtil {
 	private TestDataUtil() {}
@@ -10,6 +11,16 @@ public final class TestDataUtil {
 			.id(1L)
 			.name("John Doe")
 			.age(30)
+			.build();
+	}
+
+	public static Book createTestBook() {
+		return Book
+			.builder()
+			.id(1L)
+			.isbn("1234567890")
+			.title("Book Title")
+			.authorId(1L)
 			.build();
 	}
 }
--- src/test/java/com/oiab/database/dao/implementation/BookDaoImplIntegrationTest.java
+++ src/test/java/com/oiab/database/dao/implementation/BookDaoImplIntegrationTest.java
@@ -1,2 +1,46 @@
-package com.oiab.database.dao.implementation;public class BookDaoImplIntegrationTest {
+package com.oiab.database.dao.implementation;
+
+import com.oiab.database.TestDataUtil;
+import com.oiab.database.dao.AuthorDao;
+import com.oiab.database.domain.Author;
+import com.oiab.database.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class BookDaoImplIntegrationTest {
+	private AuthorDao authorDao;
+	private BookDaoImpl underTest;
+
+	@Autowired
+	public BookDaoImplIntegrationTest(BookDaoImpl underTest, AuthorDao authorDao) {
+		this.underTest = underTest;
+		this.authorDao = authorDao;
+	}
+
+	@Test
+	public void testThatBookCanBeCreatedAndRecalled() {
+		Author author = TestDataUtil.createTestAuthor();
+
+		// Creamos nuestro autor
+		authorDao.createAuthor(author);
+
+		// Creamos nuestro libro
+		Book book = TestDataUtil.createTestBook();
+		book.setAuthorId(author.getId());
+		underTest.createBook(book);
+		Optional<Book> result = underTest.findOneBook(book.getIsbn());
+
+		// Test
+		assertThat(result).isPresent();
+		assertThat(result.get()).isEqualTo(book);
+	}
 }
